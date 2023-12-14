8aff1d746246b9465aad16792926ca7302ce1639
devusmaarn <devusmaarn@gmail.com>
2023-12-14 03:09:16 +0100

    added register and login pages

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => 'guest'], function() {
+   Route::get('/register', Register::class)->name('register');
+   Route::get('/login', Login::class)->name('login');
 });
